de87811e7cf2ea73ae68651560194928912f343b
Lamjed CHIHAOUI <lamjed.chihaoui@gmail.com>
2018-02-23 15:31:28 +0100

    Ajout du fichier main contenant Hello world

---
+++ main.c
@@ -0,0 +1,13 @@
+//  main.c
+//  HelloWorld
+//
+//  Created by Lamjed CHIHAOUI on 23/02/2018.
+//  Copyright © 2018 Lamjed CHIHAOUI. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	printf("Hello, World!\n");
+	return 0;
+}
